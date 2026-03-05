1f59c6818a334aa4be697b431b0f6f734ed59f68
Gabriel27052 <goncalves.souza27@escola.pr.gov.br>
2026-03-05 11:41:09 -0300

    Add files via upload

---
+++ Gabriel-Goncalves-de-Souza-2B/Projeto-Hotel/Back-End/app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+
+FRONTEND_DIR = os.path.join(BASE_DIR, "Front-End")
+
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+app = Flask(__name__, static_folder=STATIC_DIR,static_url_path="/" + STATIC_DIR)
+
+@app.route("/")
+def home():
+    return send_from_directory(FRONTEND_DIR, "index.html")
+
+@app.route("/consulta")
+def consulta():
+    return send_from_directory(FRONTEND_DIR, "consulta.html")
+
+@app.route("/alterar")
+def alterar():
+    return send_from_directory(FRONTEND_DIR, "alterar.html")
+
+app.run(debug=True)

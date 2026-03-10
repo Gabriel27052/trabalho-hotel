0cb6bc5eda73eaf962525f415e5fdbd3fd59c4a6
Gabriel27052 <goncalves.souza27@escola.pr.gov.br>
2026-03-10 11:18:49 -0300

    Add files via upload

--- Gabriel-Goncalves-de-Souza-2B/Projeto-Hotel/Back-End/app.py
+++ Gabriel-Goncalves-de-Souza-2B/Projeto-Hotel/Back-End/app.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask, send_from_directory
+import openpyxl
+from datetime import(
+    datetime,
+)
 
 app = Flask(__name__)
 
@@ -9,6 +13,18 @@ FRONTEND_DIR = os.path.join(BASE_DIR, "Front-End")
 
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
+DB_DIR = os.path.join(os.path.dirname(__file__), "..", "db")
+EXCEL_FILE = os.path.join(DB_DIR, "clientes.xlsx")
+
+
+COLUMNS = [
+    "ID",
+    "Nome",
+    "Telefone",
+    "Endereço",
+    "Observações",
+    "Data Cadastro",
+]
 app = Flask(__name__, static_folder=STATIC_DIR,static_url_path="/" + STATIC_DIR)
 
 @app.route("/")
@@ -22,5 +38,10 @@ def consulta():
 @app.route("/alterar")
 def alterar():
     return send_from_directory(FRONTEND_DIR, "alterar.html")
+
+if __name__=="__main__":
+    print("BASE_DIR", BASE_DIR)
+    print("FRONTEND_DIR", FRONTEND_DIR)
+    print("STATIC_DIR", STATIC_DIR)
 
 app.run(debug=True)
